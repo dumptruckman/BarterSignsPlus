e25a4dd963266c208811225219e6a5868a168e65
Jeremy Wood <farachan@gmail.com>
2011-09-28 17:23:46 -0400

    Implemented/Updated EntityListener from BarterSigns.

---
+++ src/main/java/com/dumptruckman/bartersignsplus/listener/BarterSignsEntityListener.java
@@ -0,0 +1,40 @@
+package com.dumptruckman.bartersignsplus.listener;
+
+import com.dumptruckman.bartersignsplus.BarterSignsManager;
+import com.dumptruckman.bartersignsplus.BarterSignsPlus;
+import com.dumptruckman.bartersignsplus.object.BarterSign;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author dumptruckman
+ */
+public class BarterSignsEntityListener extends EntityListener {
+
+    @Override
+    public void onEntityExplode(EntityExplodeEvent event) {
+        if (event.isCancelled()) return;
+
+        List<Block> blocks = event.blockList();
+        List<BarterSign> barterSigns = new ArrayList<BarterSign>();
+        for (Block block : blocks) 
+            if (block.getState() instanceof Sign)
+                if (BarterSignsManager.barterSignExists(block)) {
+                    BarterSign bSign = BarterSignsManager.getBarterSign(block);
+                    barterSigns.add(bSign);
+                    if (bSign.isIndestructible()) {
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+
+        for (BarterSign bSign : barterSigns)
+            if (bSign.isDroppingItemsOnBreak())
+                bSign.dropContents();
+    }
+}
